c62fbb489bd6310631b280c3993e5f893e11221b
Kravchenko <kravchenko.web@gmail.com>
2022-04-02 14:45:45 +0300

    replaced hardcoded list with dynamic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,26 +17,16 @@ const Expenses = (props) => {
           selectedYear={year}
           onYearSelected={onYeahSelectHandler}
         />
-        <ExpenseItem
-          title={props.list[0].title}
-          amount={props.list[0].amount}
-          date={props.list[0].date}
-        />
-        <ExpenseItem
-          title={props.list[1].title}
-          amount={props.list[1].amount}
-          date={props.list[1].date}
-        />
-        <ExpenseItem
-          title={props.list[2].title}
-          amount={props.list[2].amount}
-          date={props.list[2].date}
-        />
-        <ExpenseItem
-          title={props.list[3].title}
-          amount={props.list[3].amount}
-          date={props.list[3].date}
-        />
+        {props.list.map((item, index) => {
+          return (
+            <ExpenseItem
+              key={index}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          );
+        })}
       </Card>
     </div>
   );
