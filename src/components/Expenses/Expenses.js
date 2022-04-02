e43376a6221d161c4be90fb5f3d4ede9b05fa084
Kravchenko <kravchenko.web@gmail.com>
2022-04-02 10:32:05 +0300

    Assignment 2: added expenses filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,42 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [year, setYear] = useState("");
+  const onYeahSelectHandler = (selectedYear) => {
+    setYear(selectedYear);
+  };
+  console.log(year);
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.list[0].title}
-        amount={props.list[0].amount}
-        date={props.list[0].date}
-      />
-      <ExpenseItem
-        title={props.list[1].title}
-        amount={props.list[1].amount}
-        date={props.list[1].date}
-      />
-      <ExpenseItem
-        title={props.list[2].title}
-        amount={props.list[2].amount}
-        date={props.list[2].date}
-      />
-      <ExpenseItem
-        title={props.list[3].title}
-        amount={props.list[3].amount}
-        date={props.list[3].date}
-      />
-    </Card>
+    <div>
+      <ExpensesFilter onYearSelected={onYeahSelectHandler} />
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.list[0].title}
+          amount={props.list[0].amount}
+          date={props.list[0].date}
+        />
+        <ExpenseItem
+          title={props.list[1].title}
+          amount={props.list[1].amount}
+          date={props.list[1].date}
+        />
+        <ExpenseItem
+          title={props.list[2].title}
+          amount={props.list[2].amount}
+          date={props.list[2].date}
+        />
+        <ExpenseItem
+          title={props.list[3].title}
+          amount={props.list[3].amount}
+          date={props.list[3].date}
+        />
+      </Card>
+    </div>
   );
 };
 
