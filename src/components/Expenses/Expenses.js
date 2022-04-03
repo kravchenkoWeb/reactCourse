15b0ea2314bc6ae03b0fbaba54ea7dab29c775ae
Kravchenko <kravchenko.web@gmail.com>
2022-04-03 11:47:53 +0300

    added conditions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,19 @@
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [year, setYear] = useState("2020");
-  const [filteredExpenses, setFilteredExpenses] = useState(
-    props.list.filter(
-      (expense) => expense.date.getFullYear().toString() === year
-    )
-  );
 
   const onYeahSelectHandler = (selectedYear) => {
     setYear(selectedYear);
-    setFilteredExpenses(
-      props.list.filter(
-        (expense) => expense.date.getFullYear().toString() === selectedYear
-      )
-    );
   };
 
-  console.log(filteredExpenses);
+  const filteredExpenses = props.list.filter(
+    (expense) => expense.date.getFullYear().toString() === year
+  );
 
   return (
     <div>
@@ -30,16 +22,7 @@ const Expenses = (props) => {
           selectedYear={year}
           onYearSelected={onYeahSelectHandler}
         />
-        {filteredExpenses.map((item, index) => {
-          return (
-            <ExpenseItem
-              key={index}
-              title={item.title}
-              amount={item.amount}
-              date={item.date}
-            />
-          );
-        })}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
